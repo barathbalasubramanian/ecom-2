dcdfa3d8574f8a6a51f87b5d1d89ccf91ad9486f
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2025-03-09 23:09:21 +0530

    'category-update'

--- models/Category.js
+++ models/Category.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     categoryName: { type: String, required: true },
-    categoryType: { type: String, required: true,
-        enum: ['Saree', 'Others'],
-    },
-    categotyImage: { type: String, required: true },
+    // categoryType: { type: String, required: true},
+    categoryDescription: { type: String, required: true },
+    subCategories: [{ type: String, required: true }],
+    categoryImage: { type: String, required: true },
     isDeleted: { type: Boolean, default: false },
 });
 module.exports = mongoose.model('Category', categorySchema);
