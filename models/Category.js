9a43efde0e4504cf5f026dc36dc368a0ad918a2e
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2025-03-19 19:53:11 +0530

    'cate-chan'

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
     categoryName: { type: String, required: true },
-    // categoryType: { type: String, required: true},
+    categoryType: { type: String, required: true},
+    categoryFallsOn: { type: String, required: true },
     categoryDescription: { type: String, required: true },
     subCategories: [{ type: String, required: true }],
     categoryImage: { type: String, required: true },
